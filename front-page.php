e96f7d28d5ec719f8997ed821663ba06581d0f63
Miguel Figueroa <miguelfigueroa90@gmail.com>
2021-02-04 09:49:34 -0400

    Adding front page

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<main class="container">
+    <?php if(have_posts()): ?>
+        <?php while(have_posts()): ?>
+            <?php the_post() ?>
+            <h1 class="my-3"><?= the_title() ?>!!</h1>
+            <?php the_content() ?>
+        <?php endwhile ?>
+    <?php endif ?>
+</main>
+
+<?php get_footer(); ?>
