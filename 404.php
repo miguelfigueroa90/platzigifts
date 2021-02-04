db3a374d0cb8fcfff53dbfb6ba083c78fe6d806b
Miguel Figueroa <miguelfigueroa90@gmail.com>
2021-02-04 11:08:43 -0400

    Adding 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<main class="container">
+    <div class="404page">
+        <h1>404 Not Found</h1>
+        <h2><a href="<?= home_url() ?>">Click here</a> to back to the main page.</h2>
+    </div>
+</main>
+
+<?php get_footer(); ?>
